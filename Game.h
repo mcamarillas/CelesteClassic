b105025885c46d98ec9e4b0fc5c79d0db0839915
Marc Camarillas <58046161+camarillas33@users.noreply.github.com>
2022-03-13 18:16:21 +0100

    Add files via upload

--- Game.h
+++ Game.h
@@ -5,8 +5,8 @@
 #include "Scene.h"
 
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 128
+#define SCREEN_WIDTH 512
+#define SCREEN_HEIGHT 512
 
 
 // Game is a singleton (a class with a single instance) that represents our whole application
