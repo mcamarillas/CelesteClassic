1ee88127478002b279433d8a02a0fbeeeb38438f
LimaZero <38329175+LimaZero@users.noreply.github.com>
2022-03-17 08:17:30 +0100

    Add files via upload

--- Player.cpp
+++ Player.cpp
@@ -20,8 +20,8 @@ enum PlayerAnims
 void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 	bJumping = false;
-	spritesheet.loadFromFile("images/Varied.png", TEXTURE_PIXEL_FORMAT_RGBA);
-	sprite = Sprite::createSprite(glm::ivec2(32,32), glm::vec2(0.5,0.5), &spritesheet, &shaderProgram);
+	spritesheet.loadFromFile("images/Madeline.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	sprite = Sprite::createSprite(glm::ivec2(32,32), glm::vec2(0.25,0.25), &spritesheet, &shaderProgram);
 	sprite->setNumberAnimations(4);
 	
 		sprite->setAnimationSpeed(STAND_LEFT, 8);
